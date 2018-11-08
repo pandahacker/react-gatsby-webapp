0eddfe9aca6c9df6e99f3d1d78926ee01486cd5c
Blake Albertson <albertson.blake@yahoo.com>
2018-11-08 13:50:48 -0500

    Update src/js/views/home.jsx

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../images/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export class Home extends React.Component {
